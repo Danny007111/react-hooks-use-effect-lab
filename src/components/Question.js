dbaeb91495eca70ceec771202a868e41af10e721
Danny Rodriguez <danny007111@hotmail.com>
2022-06-14 18:34:36 -0500

    Completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    // when time runs out, set answer to (false)
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return; 
+    }
+
+    // create timer
+    const timer = setTimeout(() => {
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000);
+
+    // clear up after timer & submitting to next question.
+    return function () {
+      clearTimeout(timer);
+    };
+    // when timeRemaining or onAnswered change...
+  }, [timeRemaining, onAnswered]);
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -17,6 +36,7 @@ function Question({ question, onAnswered }) {
       <h1>Question {id}</h1>
       <h3>{prompt}</h3>
       {answers.map((answer, index) => {
+        // Here is when we decide ...if the answer isCorrect!
         const isCorrect = index === correctIndex;
         return (
           <button key={answer} onClick={() => handleAnswer(isCorrect)}>
